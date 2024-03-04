5eda275e47857ca44564a015865ded5cfb20fee7
devingrischow <devingrischow@outlook.com>
2024-03-04 14:47:34 -0600

    saving change

--- STORYBOARD_TV_WEATHER_BASED/ViewController.swift
+++ STORYBOARD_TV_WEATHER_BASED/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    //Location Manager object
+    
+        //API Address to WeatherKit
+    let API_ADDRESS:String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        
     }
-
-
-}
-
+    
+    
+    ///Void Function to call to weatherKit and return and set the weather conditions
+    func getWeatherInfo(){
+        
+        //Preform the following IF the APIADDRESS is valid
+        if let url = NSURL(string: API_ADDRESS) {
+            
+            if let data = NSData(contentsOf: url as URL){
+                
+                
+                //Parse the information through
+                do{
+                    
+                    let parsedWeatherData = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String: AnyObject]
+                    
+                    //Temporary store the new dictionary
+                    let newDataDictionary = parsedWeatherData
+                    
+                    
+                    
+                    //With the NEW information, set the view labels to the information pulled
+                    
+                    
+                    
+                }catch{
+                    
+                }
+                
+                
+            }
+            //Bottom of if let -data-
+            
+            
+                
+                
+                
+            }//Bottom of url -if let-
+            
+            
+            
+    
+        
+        
+    }
+    
+    
+    
+ 
+    
+}//Bottom of Controller
