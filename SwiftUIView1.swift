4218fee605d4c73214bdbe22ee940ef8848c0ff0
devingrischow <devingrischow@outlook.com>
2024-03-04 14:49:30 -0600

    made new swiftUI file

---
+++ SwiftUIView1.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView1.swift
+//  STORYBOARD_TV_WEATHER_BASED
+//
+//  Created by Devin Grischow on 3/4/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView1()
+}
